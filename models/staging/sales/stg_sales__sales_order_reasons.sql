4430c79cfebad5e00e3dea90075612f7c225f72e
Carla Lira Rodrigues <carlaliradev@gmail.com>
2026-08-07 00:27:40 +0000

    feat: add sales order reason staging model

---
+++ models/staging/sales/stg_sales__sales_order_reasons.sql
@@ -0,0 +1,18 @@
+with source as (
+
+    select *
+    from {{ source('adventure_works', 'sales_salesorderheadersalesreason') }}
+
+),
+
+renamed as (
+
+    select
+        salesorderid as sales_order_id,
+        salesreasonid as sales_reason_id,
+        modifieddate as modified_at
+    from source
+)
+
+select * 
+from renamed
